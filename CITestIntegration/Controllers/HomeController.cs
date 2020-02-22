ccb9b69823e1b79402f3087cefcd0b048a74d082
kocertu <kocertu@emea.corpdir.net>
2020-02-22 23:48:33 +0300

    çllll

--- CITestIntegration/Controllers/HomeController.cs
+++ CITestIntegration/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace CITestIntegration.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;     //ggh
+        private readonly ILogger<HomeController> _logger;     //gghmmjj
 
         public HomeController(ILogger<HomeController> logger)
         {  
